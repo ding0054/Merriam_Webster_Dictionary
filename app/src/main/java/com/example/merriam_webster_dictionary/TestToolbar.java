4ea9bb50b40b42f4352defe0fb18bcca60be1b94
xiaoyuding <ding0054@algonquinlive.com>
2019-03-27 21:56:37 -0400

    toolbar page

---
+++ app/src/main/java/com/example/merriam_webster_dictionary/TestToolbar.java
@@ -0,0 +1,96 @@
+package com.example.merriam_webster_dictionary;
+
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TestToolbar extends AppCompatActivity {
+
+    private Toolbar tToolBar;
+    private String message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_toolbar);
+        tToolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(tToolBar);
+        message = "This is the initial message";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            //what to do when the menu item is selected:
+            case R.id.menuItem1:
+                //Show the toast immediately:
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.menuItem2:
+                //Show the toast immediately:
+                //Toast.makeText(this, "Welcome to Menu Example", Toast.LENGTH_LONG).show();
+                alertExample();
+                break;
+            case R.id.menuItem3:
+                //Show the toast immediately:
+                //Toast.makeText(this, "Welcome to Menu Example", Toast.LENGTH_LONG).show();
+                Snackbar snackbar;
+                snackbar = Snackbar.make(tToolBar, "Go Back?", Snackbar.LENGTH_LONG)
+                        .setAction("GoBack", e ->{
+                            Log.e("Menu Example", "Clicked Undo");
+                            finish();
+                        });
+                snackbar.show();
+                break;
+            case R.id.menuItem4:
+                //Show the toast immediately:
+                Toast.makeText(this, "You clicked on the overflow menu", Toast.LENGTH_LONG).show();
+
+                break;
+        }
+        return true;
+    }
+    public void alertExample()
+    {
+        View middle = getLayoutInflater().inflate(R.layout.more_message_toolbar, null);
+
+        EditText et = (EditText)middle.findViewById(R.id.view_edit_text);
+        //btn.setOnClickListener( clk -> {et.setText("You clicked my button!");});
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("The Message")
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // What to do on Accept
+                        message = et.getText().toString();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // What to do on Cancel
+                    }
+                }).setView(middle);
+
+        builder.create().show();
+    }
+}
